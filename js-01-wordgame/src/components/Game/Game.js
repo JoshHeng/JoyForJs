26926e88646853ffe1561e4f45f58da902bbd246
Josh Heng <Josh.Heng@gearset.com>
2026-01-28 16:49:45 +0000

    Word game: exercise 1

--- js-01-wordgame/src/components/Game/Game.js
+++ js-01-wordgame/src/components/Game/Game.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { sample } from '../../utils';
 import { WORDS } from '../../data';
+import GuessInput from "../GuessInput";
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -9,7 +10,13 @@ const answer = sample(WORDS);
 console.info({ answer });
 
 function Game() {
-  return <>Put a game here!</>;
+  const onGuess = (guess) => {
+    console.log({ guess });
+  }
+  
+  return (
+      <GuessInput onGuess={onGuess} />
+  )
 }
 
 export default Game;
---
+++ js-01-wordgame/src/components/GuessInput/GuessInput.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function GuessInput({ onGuess }) {
+  const [guess, setGuess] = React.useState('');
+  
+  return (
+      <form className="guess-input-wrapper" onSubmit={e => {
+        e.preventDefault();
+        onGuess(guess);
+        setGuess('');
+      }}>
+        <label htmlFor="guess-input">Enter guess:</label>
+        <input
+            id="guess-input"
+            type="text"
+            value={guess}
+            onChange={(e) => setGuess(e.target.value.toUpperCase())}
+            maxLength={5}
+            pattern="[A-Z]{5}"
+        />
+      </form>
+  );
+}
+
+export default GuessInput;
