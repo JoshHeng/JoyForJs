d8c72d1c7d50df19981a14117e78302ee678d877
Josh Heng <Josh.Heng@gearset.com>
2026-01-28 16:54:53 +0000

    Word game: ex2

--- js-01-wordgame/src/components/Game/Game.js
+++ js-01-wordgame/src/components/Game/Game.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { sample } from '../../utils';
 import { WORDS } from '../../data';
 import GuessInput from "../GuessInput";
+import GuessResults from "../GuessResults";
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -10,12 +11,18 @@ const answer = sample(WORDS);
 console.info({ answer });
 
 function Game() {
+  const [guesses, setGuesses] = React.useState([]);
+  
   const onGuess = (guess) => {
-    console.log({ guess });
+    setGuesses(prev => [...prev, { id: Math.random(), guess }]);
   }
   
   return (
-      <GuessInput onGuess={onGuess} />
+      <div>
+        <GuessResults guesses={guesses} />
+        <GuessInput onGuess={onGuess} />
+      </div>
+      
   )
 }
 
---
+++ js-01-wordgame/src/components/GuessResults/GuessResults.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Guess from "../Guess";
+
+function GuessResults({ guesses }) {
+  return (
+      <div className="guess-results">
+        {guesses.map(guess => <Guess guess={guess.guess} key={guess.id} />)}
+      </div>
+  )
+}
+
+export default GuessResults;
